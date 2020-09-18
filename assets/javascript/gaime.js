75714f0615a78f1997bb670060b2ad3aba72f9bd
HurMuse <caremplob@gmail.com>
2020-09-18 17:57:13 -0500

    Reset message added

--- assets/javascript/gaime.js
+++ assets/javascript/gaime.js
@@ -1,7 +1,11 @@
 /*let count = 0;*/
 const shotstaken1 = document.querySelector("#teamone-numshots");
 const shootButton1 = document.querySelector("#teamone-shoot-button");
+
 shootButton1.addEventListener("click", function () {
+  var audio = new Audio("./assets/sound effects/cheering1.mp3");
+  audio.play();
+
   let newCounterValue = Number(shotstaken1.innerHTML) + 1;
   shotstaken1.innerHTML = newCounterValue;
 
@@ -18,7 +22,11 @@ shootButton1.addEventListener("click", function () {
 
 const shotstaken2 = document.querySelector("#teamtwo-numshots");
 const shootButton2 = document.querySelector("#teamtwo-shoot-button");
+
 shootButton2.addEventListener("click", function () {
+  var audio = new Audio("./assets/sound effects/crowdboo.mp3");
+  audio.play();
+
   let newCounterValue = Number(shotstaken2.innerHTML) + 1;
   shotstaken2.innerHTML = newCounterValue;
 
@@ -36,21 +44,31 @@ shootButton2.addEventListener("click", function () {
 
 const resets = document.querySelector("#num-resets");
 const resetAllButton = document.querySelector("#reset-button");
+let count = 0;
+const winningTeam = document.querySelector("#winner");
+let winning1 = "Team Javascript!";
+let winning2 = "Team CSS!";
+
 resetAllButton.addEventListener("click", function () {
   let newCounterValue = Number(resets.innerHTML) + 1;
   resets.innerHTML = newCounterValue;
 
-  let count = 0;
+  const goal2 = document.querySelector("#teamtwo-numgoals");
+  const goal1 = document.querySelector("#teamone-numgoals");
+  if (goal1.innerHTML > goal2.innerHTML) winningTeam.innerHTML = winning1;
+  else winningTeam.innerHTML = winning2;
+
+  console.log(winner);
 
   const shotstaken1 = document.querySelector("#teamone-numshots");
   shotstaken1.innerHTML = count;
 
-  const goal1 = document.querySelector("#teamone-numgoals");
+  /*const goal1 = document.querySelector("#teamone-numgoals");*/
   goal1.innerHTML = count;
 
   const shotstaken2 = document.querySelector("#teamtwo-numshots");
   shotstaken2.innerHTML = count;
 
-  const goal2 = document.querySelector("#teamtwo-numgoals");
+  /*const goal2 = document.querySelector("#teamtwo-numgoals");*/
   goal2.innerHTML = count;
 });
